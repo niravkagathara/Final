ece0f291e7e75f17809045435897460d23a04613
Nirav kagathara <121386601+niravkagathara@users.noreply.github.com>
2024-06-08 14:15:32 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -90,15 +90,13 @@ const ProductList = () => {
                             {products.map((product, index) => (
                                 <>
                                     <tr   key={product._id}>
-                                        <a onClick={showedit} >
-                                            <td >{index + 1}</td>
-                                        <td style={{ height: '15rem', width: '20rem' }}><img src={`https://ecommerce-1mc7.onrender.com/images/${product.imageA}`} alt={product.name} style={{ width: '100px' }} /></td>
-                                        <td style={{ height: '6rem', width: '18rem' }}>{product.name}</td>
-                                        <td style={{ height: '6rem', width: '15rem' }}>${product.price}</td>
-                                        <td style={{ height: '10rem', width: '50rem' }}>{product.category}</td>
-                                        <td style={{ height: '6rem', width: '10rem' }}>{product.companyname}</td>
+                                            <td onClick={showedit} >{index + 1}</td>
+                                        <td onClick={showedit} style={{ height: '15rem', width: '20rem' }}><img src={`https://ecommerce-1mc7.onrender.com/images/${product.imageA}`} alt={product.name} style={{ width: '100px' }} /></td>
+                                        <td onClick={showedit} style={{ height: '6rem', width: '18rem' }}>{product.name}</td>
+                                        <td onClick={showedit} style={{ height: '6rem', width: '15rem' }}>${product.price}</td>
+                                        <td onClick={showedit} style={{ height: '10rem', width: '50rem' }}>{product.category}</td>
+                                        <td onClick={showedit} style={{ height: '6rem', width: '10rem' }}>{product.companyname}</td>
 {/*                                         <td  onClick={showedit} ><a className="icon"><i className='bx bxs-edit-alt' style={{ color: '#2140fb' }} ></i></a></td> */}
-                                        <a/>
                                             <td ><a className="icon"><i class='bx bxs-trash-alt' style={{ color: '#df0808' }} onClick={handleShow}></i></a></td>
                                     </tr>
                                     <Modal show={show} onHide={handleClose}>
